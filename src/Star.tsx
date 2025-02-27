252dcc5e4c53186847655d98cfdfaada0bdd0688
EVM27 <e.matchin@mail.ru>
2025-02-27 22:28:51 +0300

    2

--- src/Star.tsx
+++ src/Star.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 
-const Star = () => {
+const Star: React.FC = () => {
     return (
         <li>
             <svg fill="#D3BCA2" height="28" viewBox="0 0 18 18" width="28" xmlns="http://www.w3.org/2000/svg">
@@ -10,6 +10,6 @@ const Star = () => {
             </svg>
         </li>
     );
-}
+};
 
 export default Star;
